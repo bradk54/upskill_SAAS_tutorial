c83f449bb26453c507749d0a8823299ba88b8e05
Brad Kittrell <kittrellbm@gmail.com>
2017-02-19 18:18:17 +0000

    Added contact model file

---
+++ db/migrate/20170202011632_create_contacts_2.rb
@@ -0,0 +1,6 @@
+class CreateContacts2 < ActiveRecord::Migration[5.0]
+  def change
+    create_table :contacts_2s do |t|
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202000821) do
+ActiveRecord::Schema.define(version: 20170202011632) do
 
   create_table "contacts", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.text "comments"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts_2s", force: :cascade do |t|
   end
 
 end
